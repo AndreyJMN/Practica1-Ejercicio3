352d86fa25e85073d246455656b102d70bcda167
Andrey Jesús Morales Navarro <304560419@uca.ac.cr>
2025-09-22 01:53:26 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practica1_Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1000; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
